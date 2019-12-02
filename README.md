4facd3e9439abdeabde0ce673d356918a2d15afc
s49639 <s49639@eon.com>
2019-12-02 13:08:51 +0100

    reduced

--- README.md
+++ README.md
@@ -4,15 +4,12 @@ A small python based sql parser focusing on finding table dependencies within da
 The current implementation handles the parser as an seperate app that can be used to parse sql files. This is not a stable version. Within the 
 next month the goal is to translate that app in a pip package. 
 
+## minimalistic version of parsesql
+no sqlalchemy, no writing to snowflake or sqlite, plain csv
+
 ## How to use the parser:
 1. Download the repository
 2. Intialize the pipenv or generate a requirement.text and intialize it
-3. Create a json configuration file under parser/config/configuration.json (There is an example configuration available)
-4. Configure the SQLAlchemy engine
-5. Create the target database table (Sqllite or Snowflake engine) with SQLAlchemy. Therefore run: 
-```
-cd parser/
-python -m main.database.init_db
 ```
 6. Configure the Runner class (multiprocessing, parsing vs. dataloading)
 7. Run the main module with:
--- parser/app.py
+++ parser/app.py
@@ -22,19 +22,16 @@
 
 from main.sql_parser.snowsqlparser import ParseSql
 from main.sql_parser.file_finder import FileFinder
-from main.database.db_engine import Session
-from main.database.models import TableDependency
 from main.executers import SequentialExecuter, MultiProcessingExecuter
 import uuid
 import time
 
 
 class Runner(object):
-    def __init__(self, parallelism=0, bulk_load=True):
+    def __init__(self, parallelism=0):
         self.allfiles = None
         self.dependencies = list()
         self.parallelism = parallelism
-        self.bulk_load = bulk_load
         self.executer = self._get_executer()
 
     def _get_executer(self):
@@ -52,47 +49,27 @@ class Runner(object):
     def findFiles(self) -> None:    
         self.allfiles = FileFinder().getListOfFiles()
 
-    def _data_load(self):
-        if self.bulk_load:
-            self._bulkinsertdep()
-        else:
-            self._insertdep()
-
-    def _insertdep(self) -> None:
-        session = Session()
+    def _write_csv(self):
+        import tempfile, csv
+        csv_names=[]
         for sqlobject in self.dependencies:
             for table in sqlobject['tables']:
-                dbentry = TableDependency( objectName = sqlobject['name'] ,
-                                           filename = sqlobject['filename'],
-                                           dependentTableName= table,
-                                           uuid = str(uuid.uuid1())
-                                        )
-                session.add(dbentry)
-                session.commit()
-        session.close()
+                csventry = str([sqlobject['name'] ,
+                           sqlobject['filename'],
+                           table,
+                           str(uuid.uuid1())])
+                with tempfile.NamedTemporaryFile() as temp_csv:
+                    temp_csv.write(csventry)
+                csv_names.append(temp_csv.name)
 
-    def _bulkinsertdep(self) -> None:
-        session = Session()
-        bulkinsertobjects = list()
-        for sqlobject in self.dependencies:
-            for table in sqlobject['tables']:
-                dbentry = TableDependency( objectName = sqlobject['name'] ,
-                                           filename = sqlobject['filename'],
-                                           dependentTableName= table,
-                                           uuid = str(uuid.uuid1())
-                                        )
-                bulkinsertobjects.append(dbentry)
-        session.bulk_save_objects(bulkinsertobjects)
-        session.commit()
-        session.close()
 
     def start(self) -> None:
         self.parseSql()
-        self._data_load()
+        self._write_csv()
 
 if __name__ == "__main__":
     starttime = time.time()
-    Runner(parallelism=1, bulk_load=True).start()
+    Runner(parallelism=1).start()
     endtime = time.time()
     print('Time needed:', endtime - starttime )
     
